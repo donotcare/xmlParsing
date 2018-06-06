a1904e94365b6e86ba4a8c045f9cf29a64b4fd6c
Zhmylev Alexey <bezblagodatnost@gmail.com>
2018-06-06 23:48:23 +0300

    add recipes xsd schema

--- src/main/java/playground/XmlParser.java
+++ src/main/java/playground/XmlParser.java
@@ -16,19 +16,19 @@ public class XmlParser {
     private static final Logger log = Logger.getLogger(XmlParser.class.getName());
 
     public static void main(String[] args) throws IOException {
-        List<String> ingridients = new ArrayList<>();
+        List<String> ingredients = new ArrayList<>();
         URL xmlUrl = Resources.getResource("recipes.xml");
         try (StaxStreamProcessor xmlProcessor = new StaxStreamProcessor(xmlUrl.openStream())) {
             while (xmlProcessor.doUntil(XMLEvent.START_ELEMENT, "ingredient")) {
-                ingridients.add(xmlProcessor.getAttributeValue("name"));
+                ingredients.add(xmlProcessor.getAttributeValue("name"));
             }
-            log.info(ingridients::toString);
+            log.info(ingredients::toString);
         } catch (Exception e) {
             log.log(SEVERE, "Error", e);
         }
 
         XpathProcessor xpathProcessor = new XpathProcessor(xmlUrl.openStream());
         NodeList nodes = xpathProcessor.evaluate("r:collection/r:recipe/r:date");
-        System.out.println(nodes.item(1).getTextContent());
+        log.info(nodes.item(1).getTextContent());
     }
 }
